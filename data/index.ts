f151b9ea4c6e7962cd66209629f2ccb3238419c8
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-23 16:53:43 +0100

    chore: data in hompage

---
+++ data/index.ts
@@ -0,0 +1,22 @@
+export const Offers = [
+  {
+    title: "Fashion and Apparel:",
+    subtitle:
+      "Explore the latest trends in clothing, shoes, and accessories. Our fashion collection boasts a blend of classic and contemporary styles to cater to your wardrobe desires.",
+  },
+  {
+    title: "Electronics and Gadgets:",
+    subtitle:
+      "Stay ahead of the tech game with our cutting-edge electronics and gadgets. From the latest smartphones to innovative home automation, we have it all.",
+  },
+  {
+    title: "Home and Lifestyle:",
+    subtitle:
+      "Transform your living space with our carefully selected home decor and lifestyle products. Discover items that infuse personality into your surroundings and make daily life more enjoyable.",
+  },
+  {
+    title: "Gifts and More: ",
+    subtitle:
+      "Finding the perfect gift for your loved ones has never been easier. Our wide range of unique and thoughtful gift options will leave a lasting impression.",
+  },
+];
