ca201ce23916b9d9715c1fcd8d885cc25d968e5d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 09:46:02 +0100

    chore: use product Layout

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -4,7 +4,13 @@
       <h4 class="text-h4">Products Page</h4>
       <VDivider class="my-4" />
       <VRow>
-        <VCol cols="3" v-for="product in products" :key="product.id">
+        <VCol
+          cols="12"
+          md="3"
+          sm="12"
+          v-for="product in products"
+          :key="product.id"
+        >
           <ProductCard :product="product" />
         </VCol>
       </VRow>
@@ -13,8 +19,11 @@
 </template>
 
 <script setup lang="ts">
-import type { ProductType } from "~~/types";
-import { uri } from "~~/data";
+definePageMeta({
+  layout: 'product',
+});
+import type { ProductType } from '~~/types';
+import { uri } from '~~/data';
 
 // Fetch Products
 const { data: products } = await useFetch<ProductType[]>(uri);
