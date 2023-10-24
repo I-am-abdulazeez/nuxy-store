eca8a6484afe474f6b7648723a2c77607cb4c639
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 07:19:26 +0100

    chore: Wrap in ProductCard component

---
+++ pages/products/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <VContainer>
+      <h4 class="text-h4">Products Page</h4>
+      <VDivider class="my-4" />
+      <VRow>
+        <VCol cols="3" v-for="product in products" :key="product.id">
+          <ProductCard :product="product" />
+        </VCol>
+      </VRow>
+    </VContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ProductType } from "~~/types";
+import { uri } from "~~/data";
+
+// Fetch Products
+const { data: products } = await useFetch<ProductType[]>(uri);
+</script>
