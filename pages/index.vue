1bf2f6e662fab01f7fcf737b11caf567a45c64e5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 10:40:28 +0100

    refactor: prettier in action

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,12 @@
     <h4 class="text-h4">Nuxy Store</h4>
     <VDivider class="my-4" />
     <p class="text-body-1">
-      Nuxy-Store is your ultimate destination for all things fashionable, trendy, and unique. We are
-      more than just a store; we are a lifestyle brand that offers a curated selection of products
-      designed to elevate your style and enhance your everyday life. Whether you're a fashion
-      enthusiast, a tech aficionado, a home decor connoisseur, or simply looking for the perfect
-      gift, Nuxy-Store has something special for everyone.
+      Nuxy-Store is your ultimate destination for all things fashionable,
+      trendy, and unique. We are more than just a store; we are a lifestyle
+      brand that offers a curated selection of products designed to elevate your
+      style and enhance your everyday life. Whether you're a fashion enthusiast,
+      a tech aficionado, a home decor connoisseur, or simply looking for the
+      perfect gift, Nuxy-Store has something special for everyone.
     </p>
     <div class="my-6">
       <h4 class="text-h4">What we Offer</h4>
@@ -21,12 +22,5 @@
 </template>
 
 <script setup lang="ts">
-import { Offers } from "~~/data";
-
-const email = ref("");
-const password = ref("");
-
-const { ruleEmail, rulePassLen, ruleRequired } = useFormRules();
-
-const submit = async () => {};
+import { Offers } from '~~/data';
 </script>
