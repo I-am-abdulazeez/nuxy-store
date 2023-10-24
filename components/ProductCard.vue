8646b00b57c110f074155e1844df60a343c90903
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 07:17:52 +0100

    chore: ProductCard component

---
+++ components/ProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <VCard :elevation="0">
+      <VImg :src="product.image" height="300px" cover></VImg>
+      <VCardTitle>{{ product.title }}</VCardTitle>
+      <VCardSubtitle>{{ product.description }}</VCardSubtitle>
+      <VCardActions>
+        <VBtn color="info" size="small" :to="`/products/${product.id}`" nuxt variant="text">
+          View Details
+        </VBtn>
+      </VCardActions>
+    </VCard>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ProductType } from "~~/types";
+import type { PropType } from "vue";
+
+const { product } = defineProps({
+  product: {
+    type: Object as PropType<ProductType>,
+    required: true,
+  },
+});
+</script>
