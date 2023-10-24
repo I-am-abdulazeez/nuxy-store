f5bbf2d89244c3ff4954b8fd0cf15e0805c141a6
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 10:39:17 +0100

    styles: template design on error page

--- error.vue
+++ error.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex flex-column align-center justify-center h-screen">
-    <h2 class="text-h2">{{ error?.statusCode }}</h2>
-    <h5 class="text-subtitle-1">Oops something went wrong</h5>
-    <h4 class="text-h4 my-4">{{ error?.message }}</h4>
+    <h2 class="text-h2 text-center">{{ error?.statusCode }}</h2>
+    <h5 class="text-subtitle-1 text-center">Oops something went wrong</h5>
+    <h4 class="text-h4 my-4 text-center">{{ error?.message }}</h4>
     <div>
       <VBtn
         block
