f3297082265cf67505520971128f48857964dce3
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 09:46:56 +0100

    chore: Error page

---
+++ error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex flex-column align-center justify-center h-screen">
+    <h2 class="text-h2">{{ error?.statusCode }}</h2>
+    <h5 class="text-subtitle-1">Oops something went wrong</h5>
+    <h4 class="text-h4 my-4">{{ error?.message }}</h4>
+    <div>
+      <VBtn
+        block
+        color="primary"
+        size="x-small"
+        variant="tonal"
+        @click="handleError"
+        >Go home</VBtn
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue';
+import type { ErrorObject } from '~~/types';
+
+const { error } = defineProps({
+  error: Object as PropType<ErrorObject>,
+});
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
+
+<style scoped></style>
