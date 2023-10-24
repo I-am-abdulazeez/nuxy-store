164f5088f4b5c64d119ebb1d349465a1a3918f96
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 10:41:25 +0100

    chore: Meta Data

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <Head>
+      <Title>Nuxi Store | {{ product?.title }}</Title>
+      <Meta name="description" :content="product?.description" />
+    </Head>
     <h5 class="text-h5">Product Details for {{ product?.id }}</h5>
     <VDivider class="my-4" />
     <ProductDetails :product="product" />
@@ -18,4 +22,12 @@ const route = useRoute();
 const { id } = route.params;
 
 const { data: product } = await useFetch<ProductType>(`${uri}/${id}`);
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Product not found',
+    fatal: true,
+  });
+}
 </script>
