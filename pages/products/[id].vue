220d332106a8dfa0d6a82035a3526b51483a2574
AbdulAzeez <abdulazeezolamide50@gmail.com>
2023-10-24 07:19:04 +0100

    chore: ProductDetails component,

---
+++ pages/products/[id].vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <VBtn
+      icon="mdi-arrow-left"
+      size="small"
+      @click="goBack()"
+      variant="tonal"
+      color="primary"
+      class="mb-4"
+    />
+    <h5 class="text-h5">Product Details for {{ product?.id }}</h5>
+    <VDivider class="my-4" />
+
+    <ProductDetails :product="product" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { uri } from "~/data";
+import type { ProductType } from "~/types";
+
+const route = useRoute();
+const router = useRouter();
+const { id } = route.params;
+
+const goBack = () => {
+  router.back();
+};
+
+const { data: product } = await useFetch<ProductType>(`${uri}/${id}`);
+</script>
